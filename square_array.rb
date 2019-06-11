4e6ca304bf10de9dea72405c4af08e0b03f1e991
Ibrahim  Akar <ibsakar@gmail.com>
2019-06-11 02:41:26 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+
+def square_array(arr)
+  squared = []
+  arr.each_with_index do |num, index|
+    squared << num * num
+  end
+  return squared
 end
+
+
+
